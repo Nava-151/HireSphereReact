9b08e1c8c4e1e7fd87032a44e3884df6d6d62ca6
Nava-151 <n87891@gmail.com>
2025-04-25 01:37:37 +0300

    add space before footer

--- CandidteApp/src/AppLayout.tsx
+++ CandidteApp/src/AppLayout.tsx
@@ -21,13 +21,14 @@
 import { Outlet } from "react-router";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import { Box } from "@mui/material";
 
 const AppLayout = () => {
     return (
         <>
             <NavBar />
             <Outlet />
-
+            <Box sx={{ height: "350px" }}></Box>
             <Footer />
         </>
     );
--- CandidteApp/src/components/Home.tsx
+++ CandidteApp/src/components/Home.tsx
@@ -147,7 +147,6 @@ const Home = () => {
           );
         })}
       </Container>
-      <Box sx={{ height: "350px" }}></Box>
     </Box>
   );
 };
